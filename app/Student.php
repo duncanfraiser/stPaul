cfc0784a28656255ccb52fccf49638e3b66abd15
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-07-29 16:23:37 -0500

    added student

---
+++ app/Student.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Education;
+
+class Student extends Model
+{
+            use SoftDeletes;
+	      protected $fillable = [
+            'education_id',
+            'firstName',
+            'middleName',
+            'lastName',
+            'dob',
+            'age',
+            'grade',
+            'school',
+            'Date_of_Baptism',
+            'Baptism_Church',
+            'Church_Address',
+            'Communion',
+            'Communion_Church',
+            'Confirmation',
+            'Confirmation_Church',
+            'email',
+            'cell',
+            'needs'
+            ];
+
+      public function education(){
+            return $this->belongsTo('App\Education');
+      }
+}
