236d8871abd7a3a4039e131ee191b397e1e01d90
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-08-09 17:02:11 -0500

    rcia index

--- resources/views/catechumen/index.blade.php
+++ resources/views/catechumen/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <div class="col-md-11 col-md-offset-1">
-<h2>RICA Registration List
+<h2>RCIA Registration List
 <small><div class="dropdown" style="padding: 0px 0px 10px 0px">
 <button class=" dropdown-toggle" style="padding: 0px 0px 0px 0px" data-toggle="dropdown">Index Options
 <span class="caret"></span></button>
