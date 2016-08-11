6dde9148e2fb48d354f6a814079f239a23888743
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-08-11 15:48:23 -0500

    member

--- resources/views/member/create.blade.php
+++ resources/views/member/create.blade.php
@@ -1,22 +1,31 @@
-@extends('includes.layout')
+@extends('layouts.main')
 @section('content')
-
-
-<div class="DFcontainer col-md-8 col-md-offset-2">
-{{Form::open(['method' => 'POST', 'action' => 'MembersController@store'])}}
+<div class="col-md-8 col-md-offset-2">
+<div class="col-md-12">
+    @if (count($errors))
+    <ul class="err">
+    @foreach($errors->all() as $error)
+    <li class="err">{{$error}}</li>
+    @endforeach
+    </ul>
+    @endif
+</div>
+		<center><h1>Member Registration</h1></center><br>
 		<div class="form-group">
 			{{Form::label('familyName', 'Family Name:')}}
-			{{Form::text('familyName', null, ['class' => 'form-inline', 'placeholder' => 'Enter Family Name'])}}
+			{{Form::text('familyName', null, ['class' => 'form-control', 'placeholder' => 'Enter Family Name'])}}
 			{{Form::label('todaysDate', 'Todays Date:')}}
-			{{Form::input('date', 'todaysDate', null, ['class' => 'form-inline'])}}
+			{{Form::text('todaysdate', null, ['class' => 'form-control', 'placeholder' => 'MM/DD/YYYY'])}}
+			
 		</div>
 		<h3>Head of Household (Active Catholic)</h3>
 		<div class="form-group">
-			{{Form::label('tNameOne', 'Full Name:')}}
-			{{Form::select('tNameOne', ['Mr.' => 'Mr.', 'Ms.' => 'Ms.', 'Mrs.' => 'Mrs.'])}}
-			{{Form::text('fNameOne', null, ['class' => 'form-inline', 'placeholder' => 'Enter First Name'])}}
-			{{Form::text('mNameOne', null, ['class' => 'form-inline', 'placeholder' => 'Enter Middle Name'])}}
-			{{Form::text('lNameOne', null, ['class' => 'form-inline', 'placeholder' => 'Enter Last Name'])}}
+{{-- 			{{Form::label('tNameOne', 'Full Name:')}}
+			{{Form::select('tNameOne', ['Mr.' => 'Mr.', 'Ms.' => 'Ms.', 'Mrs.' => 'Mrs.'])}} --}}
+			{{Form::label('fNameOne', 'Enter Full Name:')}}
+			{{Form::text('fNameOne', null, ['class' => 'form-control', 'placeholder' => 'Enter First Name'])}}
+			{{Form::text('mNameOne', null, ['class' => 'form-control', 'placeholder' => 'Enter Middle Name'])}}
+			{{Form::text('lNameOne', null, ['class' => 'form-control', 'placeholder' => 'Enter Last Name'])}}
 		</div>		
 		<div class="form-group">
 			{{Form::label('perferedNameOne', 'Name you prefer to be callled:')}}
@@ -24,7 +33,7 @@
 		</div>
 		<div class="form-group">
 			{{Form::label('dobOne', 'Date of Birth:')}}
-			{{Form::input('date', 'dobOne', null, ['class' => 'form-control'])}}
+			{{Form::text('dobOne', null, ['class' => 'form-control', 'placeholder' => 'MM/DD/YYYY'])}}
 		</div>
 		<div class="form-group">
 			{{Form::label('maritalStat', 'Marital Status:')}}
@@ -36,11 +45,11 @@
 		</div>
 		<div class="form-group">
 			{{Form::label('homePhone', 'Home Phone:')}}
-			{{Form::text('homePhone', null, ['class' => 'form-control', 'placeholder' => 'Enter Home Phone'])}}
+			{{Form::text('homePhone', null, ['class' => 'form-control', 'placeholder' => '(###) ###-####'])}}
 		</div>
 		<div class="form-group">
 			{{Form::label('cellPhoneOne', 'Cell Phone:')}}
-			{{Form::text('cellPhoneOne', null, ['class' => 'form-control', 'placeholder' => 'Enter Cell Phone'])}}
+			{{Form::text('cellPhoneOne', null, ['class' => 'form-control', 'placeholder' => '(###) ###-####'])}}
 		</div>
 		<div class="form-group">
 			{{Form::label('emailOne', 'Primary Email Address:')}}
@@ -52,22 +61,25 @@
 		</div>
 		<div class="form-group">
 			{{Form::label('workPhoneOne', 'Work Phone:')}}
-			{{Form::text('workPhoneOne', null, ['class' => 'form-control', 'placeholder' => 'Enter Work Phone'])}}
+			{{Form::text('workPhoneOne', null, ['class' => 'form-control', 'placeholder' => '(###) ###-####'])}}
 		</div>
 		<div class="form-group">
 			{{Form::label('employmentOne', 'Place of Employment:')}}
 			{{Form::text('employmentOne', null, ['class' => 'form-control', 'placeholder' => 'Enter Place of Employment'])}}
 		</div>
+
+	<div class="form-group">
+		<h3>Spouse</h3>{{Form::label('active', 'Active Catholic?')}}<br>
+    	{{ Form::radio('active', 'Yes') }} {{ Form::label('active', 'Yes')}}<br />
+		{{ Form::radio('active', 'No') }} {{ Form::label('active', 'No')}}
+ 	</div>
 		<div class="form-group">
-			<h3>Spouse</h3>{{Form::label('active', 'Active Catholic?')}}
-            {{Form::select('active', ['No' => 'No', 'Yes' => 'Yes'])}}
-		</div>
-		<div class="form-group">
-			{{Form::label('tNameOne', 'Full Name:')}}
-			{{Form::select('tNameTwo', ['Mr.' => 'Mr.', 'Ms.' => 'Ms.', 'Mrs.' => 'Mrs.'])}}
-			{{Form::text('fNameTwo', null, ['class' => 'form-inline', 'placeholder' => 'Enter First Name'])}}
-			{{Form::text('mNameTwo', null, ['class' => 'form-inline', 'placeholder' => 'Enter Middle Name'])}}
-			{{Form::text('lNameTwo', null, ['class' => 'form-inline', 'placeholder' => 'Enter Last Name'])}}
+{{-- 			{{Form::label('tNameOne', 'Full Name:')}}
+			{{Form::select('tNameTwo', ['Mr.' => 'Mr.', 'Ms.' => 'Ms.', 'Mrs.' => 'Mrs.'])}} --}}
+			{{Form::label('fNameTwo', 'Enter Full Name:')}}
+			{{Form::text('fNameTwo', null, ['class' => 'form-control', 'placeholder' => 'Enter First Name'])}}
+			{{Form::text('mNameTwo', null, ['class' => 'form-control', 'placeholder' => 'Enter Middle Name'])}}
+			{{Form::text('lNameTwo', null, ['class' => 'form-control', 'placeholder' => 'Enter Last Name'])}}
 		</div>
 			<div class="form-group">
 			{{Form::label('perferedNameTwo', 'Name you prefer to be callled:')}}
@@ -75,11 +87,11 @@
 		</div>
 		<div class="form-group">
 			{{Form::label('dobTwo', 'Date of Birth:')}}
-			{{Form::input('date', 'dobTwo', null, ['class' => 'form-control'])}}
+			{{Form::text('dobTwo', null, ['class' => 'form-control', 'placeholder' => 'MM/DD/YYYY'])}}
 		</div>	
 		<div class="form-group">
 			{{Form::label('cellPhoneTwo', 'Cell Phone:')}}
-			{{Form::text('cellPhoneTwo', null, ['class' => 'form-control', 'placeholder' => 'Enter Cell Phone'])}}
+			{{Form::text('cellPhoneTwo', null, ['class' => 'form-control', 'placeholder' => '(###) ###-####'])}}
 		</div>
 		<div class="form-group">
 			{{Form::label('emailTwo', 'Primary Email Address:')}}
@@ -91,15 +103,13 @@
 		</div>
 		<div class="form-group">
 			{{Form::label('workPhoneTwo', 'Work Phone:')}}
-			{{Form::text('workPhoneTwo', null, ['class' => 'form-control', 'placeholder' => 'Enter Work Phone'])}}
+			{{Form::text('workPhoneTwo', null, ['class' => 'form-control', 'placeholder' => '(###) ###-####'])}}
 		</div>
 		<div class="form-group">
 			{{Form::label('employmentTwo', 'Place of Employment:')}}
 			{{Form::text('employmentTwo', null, ['class' => 'form-control', 'placeholder' => 'Enter Place of Employment'])}}
 		</div>	
-		<div class="form-group">
-            {{ Form::checkbox('okPhone', 'delete') }} {!! Form::label('deleated_at', 'Delete Employee') !!}            
-        </div>		
+	
 
 
 		<div class="form-group">
