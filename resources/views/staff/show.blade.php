d10af5fded1db2da32f08c09572b35c5041d929e
Duncan Fraiser <duncan.fraiser@ergon.com>
2016-12-20 14:24:02 -0600

    height

--- resources/views/staff/show.blade.php
+++ resources/views/staff/show.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
 <div class='text-center'>
 <h1>St. Paul Staff!!!</h1>
-<img style="width: 50%"  src="{{url($staff->img_path)}}">
+<img style="height: 50%"  src="{{url($staff->img_path)}}">
 <h3>{{$staff->name}}</h3>
 <a style="font-size: 24px" href="mailto:{{$staff->email}}">{{$staff->email}}</a>
 <h3>{{$staff->phone}}</h3>
