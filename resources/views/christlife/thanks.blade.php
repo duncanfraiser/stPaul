0b1f37e371ad971fa56992d70df3a21765a97a09
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-01-12 17:00:12 -0600

    DC8

--- resources/views/christlife/thanks.blade.php
+++ resources/views/christlife/thanks.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <div class="col-md-8 col-md-offset-2">
-<center><img width='100%' src="{{url('/img/Following Christ masthead.png/')}}"></center>
+<center><img width='100%' src="{{url('/banners/Discovering-Christ-logo.png/')}}"></center>
 <center><h1>Thank You {{$christlife->firstName}}!</h1></center>
 <center><h3>You are now registered for our Discovering Christ Sessions!<br />We will contact you soon with more information.</h3></center>
 </div>
