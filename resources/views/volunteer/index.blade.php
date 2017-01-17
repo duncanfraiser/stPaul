9c805a3f7342639734ebfa304e164909cd2350b7
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-01-17 09:49:27 -0600

    DC15

--- resources/views/volunteer/index.blade.php
+++ resources/views/volunteer/index.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.main')
 @section('content')
 <div class="col-md-8 col-md-offset-2"><br />
-<h1>Volunteer Index</h1>
+<h1>Volunteer Index<small><div class="dropdown" style="padding: 0px 0px 10px 0px">
+<button class=" dropdown-toggle" style="padding: 0px 0px 0px 0px" data-toggle="dropdown">Index Options
+<span class="caret"></span></button>
+<ul class="dropdown-menu">
+<li><a href="{{url('/volunteer/changes')}}">Change Report</a></li>
+</ul></div></small></h1>
 <ol>
 	@foreach($volunteers as $volunteer)
 	<li><a href="{{url('/volunteer/'.$volunteer->id)}}">{{$volunteer->last_name}}, {{$volunteer->first_name}}</a></li>
