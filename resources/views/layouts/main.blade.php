7efff4c39181dd89ee304108368dd9e55abb0f7b
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-03-21 10:02:54 -0500

    close reg

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -78,6 +78,7 @@
 
 
 <body>
+  <div class="loader"></div>
 
     <nav class="navbar navbar-fixed-top" role="navigation">
         <div class="container">
@@ -196,7 +197,12 @@
     <!-- /.container -->
 
 
-
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+    $(".loader").fadeOut("slow");
+})
+</script>
  
     @yield('scripts')
 
