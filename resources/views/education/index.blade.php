f46264d8d12b45bb0d3180d34130d3c4489119de
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-06-23 17:09:03 -0500

    table family

--- resources/views/education/index.blade.php
+++ resources/views/education/index.blade.php
@@ -1,32 +1,46 @@
 @extends('layouts.main')
 @section('content')
-<div class="col-md-11 col-md-offset-1">
-<h2>Religious Education Registration Index
-<small><div class="dropdown" style="padding: 0px 0px 10px 0px">
-<button class=" dropdown-toggle" style="padding: 0px 0px 0px 0px" data-toggle="dropdown">Index Options
-<span class="caret"></span></button>
-<ul class="dropdown-menu">
-<li><a href="{{url('/education/changes')}}">Change Report</a></li>
-<li><a href="{{url('/education/grade')}}">Sort by Grade</a></li>
-<li><a href="{{url('/education/familyEmail')}}">Family Email List</a></li>
-<li><a href="{{url('/education/studentEmail')}}">Student Email List</a></li>
+<div class="col-md-8 col-md-offset-2" style="margin-top: 50px">
+  <h2>Religious Education Registration Index
+  <small><div class="dropdown" style="padding: 0px 0px 10px 0px">
+  <button class=" dropdown-toggle" style="padding: 0px 0px 0px 0px" data-toggle="dropdown">Index Options
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="{{url('/education/changes')}}">Change Report</a></li>
+    <li><a href="{{url('/education/grade')}}">Sort by Grade</a></li>
+    <li><a href="{{url('/education/familyEmail')}}">Family Email List</a></li>
+    <li><a href="{{url('/education/studentEmail')}}">Student Email List</a></li>
+ </ul></div></small></h2>
 
-</ul></div></small></h2>
- <div class="col-md-5">                   
-	@foreach($eds as $key=>$ed)
-	{{++$key}}.
-	@if($ed->father_last_name != NULL)
-	<a href="{{url('/education/'.$ed->id.'/family')}}">{{$ed->father_last_name}} Family  </a><span style="color:red">{{ $ed->paid}}</span><br />
-	@else
-	<a href="{{url('/education/'.$ed->id.'/family')}}">{{$ed->mother_last_name}} Family  </a><span style="color:red">{{ $ed->paid}}</span><br />
-	@endif
-	<ul>
-	    @foreach($ed->student as $stud)
-    <a href="{{url('/education/'.$ed->id.'/family/'.$stud->id)}}"><li>{{$stud->firstName}} {{$stud->middleName}} {{$stud->lastName}}</li></a>
-    @endforeach   
 
-	</ul>
+<table>
+	<tr>
+	  <th class="thBlue"><center>Family</center></th>
+	  <th class="thBlue"><center>Enrolled Children</center></th>
+	  <th class="thBlue"><center>Payment Status</center></th>
+	</tr>  
+	@foreach($eds as $ed)
+	  <tr>
+	  	@if($ed->father_last_name != "")
+	      <td><a class="tdBlack" href="{{url('/education/'.$ed->id.'/family')}}">{{$ed->father_last_name}} Family</a></td>
+	    @else
+	      <td><a class="tdBlack" href="{{url('/education/'.$ed->id.'/family')}}">{{$ed->mother_last_name}} Family</a></td>
+	    @endif 
+	    <td class="tdBlue">
+		    @foreach($ed->student as $stud)
+		    <li><a href="{{url('/education/'.$stud->education_id.'/family/'.$stud->id)}}">{{$stud->firstName}} {{$stud->middleName}} {{$stud->lastName}}</a></li>
+		    @endforeach   
+		</td>
+ 
+	    @if($ed->paid != "")
+	      <td class="tdCash">{{$ed->paid}}</td>
+	    @else
+	      <td class="tdRed">No Payment Recived</td>
+	    @endif
+	  </tr>
 	@endforeach
-</div>
+</table>
+
+
 </div>
 @stop
