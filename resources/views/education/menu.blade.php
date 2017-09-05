f60e74cb05d7e7678ec0d7bae6b294d36c4d46ff
Duncan Fraiser <duncan.fraiser@ergon.com>
2017-09-05 14:14:47 -0500

    closed rel ed

--- resources/views/education/menu.blade.php
+++ resources/views/education/menu.blade.php
@@ -3,7 +3,15 @@
 <div class="col-md-8 col-md-offset-2"><br/>
 <img class="banner" src="{{url('/banners/ReligiousEdBanner.png/')}}">
 </div>
-  <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 text-center">
+<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 text-center">
+<h1><u>Online Registration is now closed.</u></h1>
+
+<p><i>Please call the Church Office @ 601-992-9547 to enroll a new or returning student for the 2017/2018 year.<br/><br/>
+
+3 yrs~6th grade classes begin Sunday, September 10th @ 9:15 am<br/>
+Big Deal Youth Group (7th~12th grade) begins Wednesday, September 13th @ 6:30 pm</i></p>
+</div>
+{{--   <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 text-center">
 	  <div class="row">
 	    <div class="col-md-6 col-sm-6 col-xs-12 bigBtnDiv">
 		  <a href="{{url('/education/create')}}"><button class="bigBlueBtn">New Family & Student</button></a>
@@ -18,5 +26,5 @@
 		  <a href="{{url('/education/about')}}"><button class="bigRedBtn">About Religious Education</button></a>
 	    </div>
 	  </div>  
-  </div>
+  </div> --}}
 @stop
